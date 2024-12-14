82fbd7a72c8cb914625b3982aac436560f382ee6
melissaceee <cheng.melissa@outlook.com>
2024-12-14 13:39:29 -0500

    update to tik tac toe

--- exercises.py
+++ exercises.py
@@ -1,23 +1,15 @@
-class Game():
-   def __init__(self):
+class Game:
+    def __init__(self):
         self.turn = 'X'
         self.tie = False
         self.winner = None
         self.board = {
             'a1': None, 'b1': None, 'c1': None,
             'a2': None, 'b2': None, 'c2': None,
-            'a3': None, 'b3': None, 'c3': None,
-    def play_game(self):
-        print('Tic-Tac-Toe')
-        while not self.tie and not self.winner:
-            self.render()
-            self.get_move()
-            self.check_for_winner()            
-            self.check_for_tie()            
-            self.switch_turn()
-            print(self.tie, self.winner)
-        self.render()
+            'a3': None, 'b3': None, 'c3': None
+        }
 
+    def welcome_message(self):
+        print("Welcome to Tic-Tac-Toe!")
 
-game_instance = Game()
-game_instance.play_game()
+  
