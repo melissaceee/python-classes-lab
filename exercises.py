7776ab3c10583f3de09ca92058e7a9524fef1ac5
melissaceee <cheng.melissa@outlook.com>
2024-12-14 13:13:08 -0500

    update initialize

--- exercises.py
+++ exercises.py
@@ -6,4 +6,4 @@ class Game():
         self.board = {
             'a1': None, 'b1': None, 'c1': None,
             'a2': None, 'b2': None, 'c2': None,
-            'a3': None, 'b3': None, 'c3': None
+            'a3': None, 'b3': None, 'c3': None,
