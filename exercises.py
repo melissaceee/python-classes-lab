0bf427be5c840263b1f831403f5f307745a29bff
melissaceee <cheng.melissa@outlook.com>
2024-12-14 13:14:47 -0500

    game instance

--- exercises.py
+++ exercises.py
@@ -7,3 +7,17 @@ class Game():
             'a1': None, 'b1': None, 'c1': None,
             'a2': None, 'b2': None, 'c2': None,
             'a3': None, 'b3': None, 'c3': None,
+    def play_game(self):
+        print('Tic-Tac-Toe')
+        while not self.tie and not self.winner:
+            self.render()
+            self.get_move()
+            self.check_for_winner()            
+            self.check_for_tie()            
+            self.switch_turn()
+            print(self.tie, self.winner)
+        self.render()
+
+
+game_instance = Game()
+game_instance.play_game()
