37e80d4132fb52d329ba41e1d92e75997c6772f8
melissaceee <cheng.melissa@outlook.com>
2024-12-14 15:06:15 -0500

    submit final tic tac toe

--- exercises.py
+++ exercises.py
@@ -43,9 +43,52 @@ class Game():
         self.print_board()
         self.print_message()
     
-  def get_move(self):
-        if (not self.tie) 
- 
+    def get_move(self):
+        if (not self.tie) or (self.winner == None):  
+        
+            move = input(f"Enter a valid move (example: A1): ").lower()
+        if move in self.board and not self.board[move]:
+            self.board[move] = self.turn
+            print(f'You picked {move}')
+        else:
+            print(f"{move} is not a valid input please try again!")
+            
+
+    def check_for_winner(self):
+        if self.board['a1'] and (self.board['a1'] == self.board['b1'] == self.board['c1']):
+            self.winner = self.turn
+        elif self.board['a2'] and (self.board['a2'] == self.board['b2'] == self.board['c2']):
+            self.winner = self.turn        
+        elif self.board['a3'] and (self.board['a3'] == self.board['b3'] == self.board['c3']):
+            self.winner = self.turn 
+        elif self.board['a1'] and (self.board['a1'] == self.board['a2'] == self.board['a3']):
+            self.winner = self.turn 
+        elif self.board['b1'] and (self.board['b1'] == self.board['b2'] == self.board['b3']):
+            self.winner = self.turn 
+        elif self.board['c1'] and (self.board['c1'] == self.board['c2'] == self.board['c3']):
+            self.winner = self.turn 
+
+    def check_for_tie(self):
+        
+        board_full = 9
+        spaces_filled = 0        
+        for space in self.board:
+            if self.board[space] == None and not self.winner:
+                pass
+            else:
+                spaces_filled += 1       
+          
+            
+        if spaces_filled == 9 and not self.winner:
+            self.tie = True
+        
+        
+                
+    def switch_turn(self):
+        if self.turn == 'X':
+            self.turn = 'O'
+        elif self.turn == 'O':
+            self.turn = 'X'
                 
                 
 
